588238d6357ce323aed159b021480d96026b2e9e
Bjoern Rudner <rudnerbjoern@hotmail.com>
2024-08-28 15:58:38 +0200

    add complementary attributes

--- dictionaries/de.dict.br-iot.php
+++ dictionaries/de.dict.br-iot.php
@@ -21,6 +21,7 @@ Dict::Add('DE DE', 'German', 'Deutsch', array(
 Dict::Add('DE DE', 'German', 'Deutsch', array(
     'Class:IOTCustomer' => 'IoT Kunde',
     'Class:IOTCustomer+' => '',
+    'Class:IOTCustomer/ComplementaryName' => '%1$s (%2$s)',
     'Class:IOTCustomer/Attribute:name' => 'Name',
     'Class:IOTCustomer/Attribute:name+' => '',
     'Class:IOTCustomer/Attribute:org_id' => 'Organisation',
@@ -43,6 +44,7 @@ Dict::Add('DE DE', 'German', 'Deutsch', array(
 Dict::Add('DE DE', 'German', 'Deutsch', array(
     'Class:IOTGateway' => 'IoT Gateway',
     'Class:IOTGateway+' => '',
+    'Class:IOTGateway/ComplementaryName' => '%1$s',
     'Class:IOTGateway/Attribute:device_id' => 'Device ID',
     'Class:IOTGateway/Attribute:device_id+' => 'Muss eine gültige GUID sein',
     'Class:IOTGateway/Attribute:cert_created' => 'Zertifikat erstellt',
@@ -96,6 +98,7 @@ Dict::Add('DE DE', 'German', 'Deutsch', array(
 Dict::Add('DE DE', 'German', 'Deutsch', array(
     'Class:IOTSensor' => 'IoT Sensor',
     'Class:IOTSensor+' => '',
+    'Class:IOTSensor/ComplementaryName' => '%1$s - %2$s',
     'Class:IOTSensor/Attribute:iotgateway_list' => 'Gateway',
     'Class:IOTSensor/Attribute:iotgateway_list+' => '',
     'Class:IOTSensor/Attribute:collection' => 'Collection',
@@ -112,6 +115,7 @@ Dict::Add('DE DE', 'German', 'Deutsch', array(
 Dict::Add('DE DE', 'German', 'Deutsch', array(
     'Class:IOTCable' => 'IoT Kabel',
     'Class:IOTCable+' => '',
+    'Class:IOTCable/ComplementaryName' => '%1$s',
     'Class:IOTCable/Attribute:iotgateway_id' => 'Gateway',
     'Class:IOTCable/Attribute:iotgateway_id+' => '',
 ));
@@ -169,6 +173,7 @@ Dict::Add('DE DE', 'German', 'Deutsch', array(
 //
 Dict::Add('DE DE', 'German', 'Deutsch', array(
     'Class:IOTSimCard' => 'SIM Karte',
+    'Class:IOTSimCard/ComplementaryName' => '%1$s',
     'Class:IOTSimCard/Attribute:sim' => 'SIM Nummer',
     'Class:IOTSimCard/Attribute:iotgateways_list' => 'IoT Gateways',
     'Class:IOTSimCard/Attribute:phone_number' => 'Mobilnummer',
